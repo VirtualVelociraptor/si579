0a0dbdcbf32b301c07c1ea73c67c66ef22cedaf1
Christian Gutierrez <gutierrezchristianf@gmail.com>
2021-02-20 12:55:09 -0800

    table mountain text

--- ps4/js/about.js
+++ ps4/js/about.js
@@ -1,5 +1,6 @@
 const cards = document.querySelectorAll(".card_single");
 const compcards = document.querySelectorAll(".comp_card_single");
+let firstFlip = true;
 
 cards.forEach((card) => card.addEventListener("click", flipCard));
 
@@ -8,7 +9,18 @@ compcards.forEach((compcard) => compcard.addEventListener("mouseenter", flipComp
 compcards.forEach((compcard) => compcard.addEventListener("mouseleave", flipCompCardLeave));
 
 function flipCard() {
+
   this.classList.toggle("flip");
+  if(firstFlip){
+    let tablmnt = document.createElement("h2");
+    let node = document.createTextNode("Front view of Table Mountain");
+    tablmnt.appendChild(node);
+  
+    let element = document.getElementById("back-of-card");
+    element.appendChild(tablmnt);
+    firstFlip = false;
+  }
+
 }
 
 function flipCompCardEnter() {
