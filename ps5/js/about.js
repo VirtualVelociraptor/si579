0c953e797cd15a444cea9c2a947b59a43ecfe048
Christian Gutierrez <gutierrezchristianf@gmail.com>
2021-02-26 19:58:47 -0800

    adding ps5 starter files

---
+++ ps5/js/about.js
@@ -0,0 +1,39 @@
+const cards = document.querySelectorAll(".card_single");
+const compcards = document.querySelectorAll(".comp_card_single");
+let firstFlip = true;
+
+cards.forEach((card) => card.addEventListener("click", flipCard));
+
+compcards.forEach((compcard) => compcard.addEventListener("mouseenter", flipCompCardEnter));
+
+compcards.forEach((compcard) => compcard.addEventListener("mouseleave", flipCompCardLeave));
+
+function flipCard() {
+
+  this.classList.toggle("flip");
+  if(firstFlip){
+    let tablmnt = document.createElement("h3");
+    let node = document.createTextNode("Front view of Table Mountain");
+    tablmnt.appendChild(node);  
+    let element = document.getElementById("back-of-card");
+    element.appendChild(tablmnt);
+
+
+    firstFlip = false;
+  }
+
+}
+
+function flipCompCardEnter() {
+    this.classList.toggle("flip");
+    this.style.backgroundColor = "#990000";
+    this.style.color = "white";
+  }
+
+  function flipCompCardLeave() {
+    this.classList.toggle("flip");
+    this.style.backgroundColor = "white";
+  }
+
+
+
