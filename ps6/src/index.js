8f9beddbfe8354374aef0dde5dd3ecad587c9c88
Christian Gutierrez <gutierrezchristianf@gmail.com>
2021-03-05 11:47:19 -0800

    placing aframe url back in index.html

--- ps6/src/index.js
+++ ps6/src/index.js
@@ -23,8 +23,9 @@
 // <script src="https://rawgit.com/protyze/aframe-alongpath-component/master/dist/aframe-alongpath-component.min.js"></script> 
 
 import aframe from 'aframe';
-//require('aframe');
+require('aframe');
 
+///////////////// HTML ////////////////////////
 const cards = document.querySelectorAll(".card_single");
 const compcards = document.querySelectorAll(".comp_card_single");
 let firstFlip = true;
@@ -45,7 +46,6 @@ function flipCard() {
     let element = document.getElementById("back-of-card");
     element.appendChild(tablmnt);
 
-
     firstFlip = false;
   }
 
@@ -63,70 +63,68 @@ function flipCompCardEnter() {
   }
 
 ////////////////////////////////////
-// SOURCE: SUPER HANDS 
+// SOURCE: A-FRAME SUPER HANDS 
 
-  // turn controller's physics presence on only while button held down
-  aframe.registerComponent("phase-shift", {
-    init: function () {
-      console.warn("installing phase shift");
-      var el = this.el;
-      el.addEventListener("gripdown", function () {
-        el.setAttribute("collision-filter", {
-          collisionForces: true,
-        });
+// turn controller's physics presence on only while button held down
+AFRAME.registerComponent("phase-shift", {
+  init: function () {
+    console.warn("installing phase shift");
+    var el = this.el;
+    el.addEventListener("gripdown", function () {
+      el.setAttribute("collision-filter", {
+        collisionForces: true,
       });
-      el.addEventListener("gripup", function () {
-        el.setAttribute("collision-filter", {
-          collisionForces: false,
-        });
+    });
+    el.addEventListener("gripup", function () {
+      el.setAttribute("collision-filter", {
+        collisionForces: false,
       });
-    },
-  });
+    });
+  },
+});
 
+// Component to change to a sequential color on click.
+AFRAME.registerComponent("cursor-listener", {
+  init: function () {
+    var lastIndex = -1;
+    var COLORS = ["red", "green", "blue", "white"];
+    this.el.addEventListener("fusing", function (evt) {
+      lastIndex = (lastIndex + 1) % COLORS.length;
+      this.setAttribute("material", "color", COLORS[lastIndex]);
+      console.log(
+        "I was clicked at: ",
+        evt.detail.intersection.point
+      );
+    });
+  },
+});
 
+//////////////////// THREE JS ///////////////////////////
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera(
+  75,
+  window.innerWidth / window.innerHeight,
+  0.1,
+  1000
+);
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize(window.innerWidth / 2, window.innerHeight / 2);
+document.body.appendChild(renderer.domElement);
 
-  // Component to change to a sequential color on click.
-  aframe.registerComponent("cursor-listener", {
-    init: function () {
-      var lastIndex = -1;
-      var COLORS = ["red", "green", "blue", "white"];
-      this.el.addEventListener("fusing", function (evt) {
-        lastIndex = (lastIndex + 1) % COLORS.length;
-        this.setAttribute("material", "color", COLORS[lastIndex]);
-        console.log(
-          "I was clicked at: ",
-          evt.detail.intersection.point
-        );
-      });
-    },
-  });
+const geometry = new THREE.BoxGeometry();
+const material = new THREE.MeshBasicMaterial({ color: 0x990000 });
+const cube = new THREE.Mesh(geometry, material);
+scene.add(cube);
 
-  //////////////////// THREE JS ///////////////////////////
-  const scene = new THREE.Scene();
-  const camera = new THREE.PerspectiveCamera(
-    75,
-    window.innerWidth / window.innerHeight,
-    0.1,
-    1000
-  );
-  const renderer = new THREE.WebGLRenderer();
-  renderer.setSize(window.innerWidth / 2, window.innerHeight / 2);
-  document.body.appendChild(renderer.domElement);
+camera.position.z = 2;
 
-  const geometry = new THREE.BoxGeometry();
-  const material = new THREE.MeshBasicMaterial({ color: 0x990000 });
-  const cube = new THREE.Mesh(geometry, material);
-  scene.add(cube);
-
-  camera.position.z = 2;
-
-  function animate() {
-    requestAnimationFrame(animate);
-    cube.rotation.x += 0.01;
-    cube.rotation.y += 0.01;
-    renderer.render(scene, camera);
-  }
-  animate();
+function animate() {
+  requestAnimationFrame(animate);
+  cube.rotation.x += 0.01;
+  cube.rotation.y += 0.01;
+  renderer.render(scene, camera);
+}
+animate();
 
 
 
